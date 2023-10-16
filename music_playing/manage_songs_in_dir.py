ad878a204a16c261c3208af2e10a0e5b93287597
Itamar <s@s.com>
2023-10-16 15:57:46 +0300

    uses pydub to calc total chunks, works now

--- music_playing/manage_songs_in_dir.py
+++ music_playing/manage_songs_in_dir.py
@@ -3,7 +3,7 @@ import wave
 import os
 from music_playing.song_class import SongInfo
 import math
-
+from pydub import AudioSegment
 
 def get_song_list(song_dir, chunk):
     return [get_song_info(song_dir, song_name, chunk) for song_name in os.listdir(song_dir)]
@@ -14,17 +14,20 @@ def get_name_to_songinfo_dict(song_dir, chunk):
 
 def get_song_info(song_dir, song_name, chunk):
     song_path = os.path.join(song_dir, song_name)
-    with wave.open(song_path) as mywav:
-        nframes = mywav.getnframes()
-        framerate = mywav.getframerate()
-        nchannels = mywav.getnchannels()
-        
+    audio = AudioSegment.from_wav(song_path)
+    nframes = audio.frame_count()
+    framerate = audio.frame_rate
+    nchannels = audio.channels
+
     length = nframes / framerate
     max_seq = math.ceil(nframes / chunk)
-    song_data = SongInfo(name=song_name, nframes=nframes, framerate=framerate, nchannels=nchannels, length=length, max_seq=max_seq)
-    logging.debug(f"{song_data=}")
-    return song_data
+    song_info = SongInfo(name=song_name, nframes=nframes, framerate=framerate, nchannels=nchannels, length=length, max_seq=max_seq)
+    logging.debug(f"{song_info=}")
+    return song_info
 
 
-    
+def calc_total_chunks(song_dir, song_name, chunk):
+    song_path = os.path.join(song_dir, song_name)
+    audio = AudioSegment.from_wav(song_path)
+    return math.ceil(audio.frame_count() / chunk)
     
--- testing.py
+++ testing.py
@@ -1,6 +1,40 @@
+import wave
+import math
+import os
 from custom_logging import CustomLogger
 import logging
+from pydub import AudioSegment
 
 testing_logger = CustomLogger(log_files=["testing.log"])
 
-logging.info(f"{bool([])=}")
+CHUNK = 4096
+
+def calc_total_chunks(song_dir, song_name, chunk):
+    song_path = os.path.join(song_dir, song_name)
+    audio = AudioSegment.from_wav(song_path)
+    return math.ceil(audio.frame_count() / chunk)
+
+songs = ["american.wav", "beats.wav", "No_38.wav"]
+
+
+def count_chunks_in_song(song_dir, song_name, chunk):
+    song_path = os.path.join(song_dir, song_name)
+    chunk_count = 0
+
+    with wave.open(song_path) as mywav:
+        while True:
+            song_data_chunk = mywav.readframes(chunk)
+            if not song_data_chunk:
+                break
+            chunk_count += 1
+
+    return chunk_count
+
+
+songs = ["warm_hall.wav", "hall.wav", "american.wav", "beats.wav", "No_38.wav"]
+
+for song in songs:
+    print(f"total chunks calc for {song}: {calc_total_chunks('songs', song, CHUNK)}")
+    print(f"total chunks read for {song}: {count_chunks_in_song('songs', song, CHUNK)}")
+
+
