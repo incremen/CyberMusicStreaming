0065972dc0ccb97ba5f79f8ce8591dc4e255118d
Itamar <s@s.com>
2023-10-21 18:47:44 +0300

    works with ngrok (renamed variables)

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,8 @@
-# server_address_str = "http://localhost:5000"
-# server_address_tuple = ('localhost', 5000)
-
-# client_connects_to_str = "tcp://6.tcp.eu.ngrok.io:12883"
-
-client_connects_to_str = "tcp://localhost:12345"
-
+server_addr_str = "http://localhost:12345"
 server_addr_tuple = ('localhost', 12345)
+
+client_connects_to_str = "tcp://2.tcp.eu.ngrok.io:12828"
+
+# client_connects_to_str = "tcp://localhost:12345"
+
+# server_addr_tuple = ('localhost', 12345)
