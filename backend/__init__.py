24d39bb014388dccddc6537ff29b134ad9b6465a
Itamar <s@s.com>
2023-10-22 12:36:21 +0300

    added back btn and gave btns icons

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,7 @@
-server_addr_str = "http://localhost:12345"
-server_addr_tuple = ('localhost', 12345)
-
-client_connects_to_str = "tcp://2.tcp.eu.ngrok.io:12828"
-
-# client_connects_to_str = "tcp://localhost:12345"
-
 # server_addr_tuple = ('localhost', 12345)
+
+# client_connects_to_str = "tcp://2.tcp.eu.ngrok.io:12828"
+
+client_connects_to_str = "tcp://localhost:12345"
+
+server_addr_tuple = ('localhost', 12345)
--- frontend/main_page_ui.py
+++ frontend/main_page_ui.py
@@ -18,7 +18,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1180, 843)
+        MainWindow.resize(1180, 834)
         sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(1)
         sizePolicy.setVerticalStretch(0)
@@ -48,15 +48,45 @@ class Ui_MainWindow(object):
         self.song_progress.setGeometry(QRect(120, 680, 511, 23))
         self.song_progress.setValue(0)
         self.song_progress.setInvertedAppearance(True)
-        self.pause_btn = QPushButton(self.main_widget)
-        self.pause_btn.setObjectName(u"pause_btn")
-        self.pause_btn.setGeometry(QRect(200, 620, 111, 41))
         self.skip_btn = QPushButton(self.main_widget)
         self.skip_btn.setObjectName(u"skip_btn")
-        self.skip_btn.setGeometry(QRect(400, 620, 111, 41))
+        self.skip_btn.setGeometry(QRect(640, 670, 81, 61))
+        self.skip_btn.setStyleSheet(u"background-color: transparent;\n"
+"border: none;\n"
+"qproperty-iconSize: 50px;\n"
+"\n"
+"")
+        icon = QIcon()
+        icon.addFile(u"../../../Downloads2/play_button_right.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.skip_btn.setIcon(icon)
+        self.skip_btn.setFlat(True)
         self.song_queue = QListWidget(self.main_widget)
         self.song_queue.setObjectName(u"song_queue")
         self.song_queue.setGeometry(QRect(785, 20, 271, 721))
+        self.back_btn = QPushButton(self.main_widget)
+        self.back_btn.setObjectName(u"back_btn")
+        self.back_btn.setGeometry(QRect(50, 660, 81, 61))
+        self.back_btn.setStyleSheet(u"background-color: transparent;\n"
+"border: none;\n"
+"qproperty-iconSize: 50px;\n"
+"\n"
+"")
+        icon1 = QIcon()
+        icon1.addFile(u"../../../Downloads2/play_button_left.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.back_btn.setIcon(icon1)
+        self.back_btn.setFlat(True)
+        self.pause_btn = QPushButton(self.main_widget)
+        self.pause_btn.setObjectName(u"pause_btn")
+        self.pause_btn.setGeometry(QRect(360, 620, 81, 61))
+        self.pause_btn.setStyleSheet(u"background-color: transparent;\n"
+"border: none;\n"
+"qproperty-iconSize: 50px;\n"
+"\n"
+"")
+        icon2 = QIcon()
+        icon2.addFile(u"../../../Downloads2/pause_icon.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pause_btn.setIcon(icon2)
+        self.pause_btn.setFlat(True)
 
         self.horizontalLayout.addWidget(self.main_widget)
 
@@ -80,7 +110,8 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.pause_btn.setText(QCoreApplication.translate("MainWindow", u"pause", None))
-        self.skip_btn.setText(QCoreApplication.translate("MainWindow", u"skip", None))
+        self.skip_btn.setText("")
+        self.back_btn.setText("")
+        self.pause_btn.setText("")
     # retranslateUi
 
