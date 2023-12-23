452cf4e2d1f281f6f2f66002000c258eebd47428
Itamar <s@s.com>
2023-12-23 19:44:46 +0200

    updated funcs so resource_file_qrc gets album pics

--- ui/generate_qrc.py
+++ ui/generate_qrc.py
@@ -1,19 +1,24 @@
 import os
 
-def generate_qrc(directory, qrc_file):
-  with open(qrc_file, 'w') as f:
-      f.write('<RCC>\n')
-      f.write(' <qresource prefix="/">\n')
-      for filename in os.listdir(directory):
-          f.write(f' <file>images\{filename}</file>\n')
-      f.write(' </qresource>\n')
-      f.write('</RCC>\n')
+import os
 
-generate_qrc(
-    r'D:\VS Code Projects (good place)\Cyber Music Streaming\ui\images', 
-    
-    r'D:\VS Code Projects (good place)\Cyber Music Streaming\ui\resource_file.qrc'
-    )
+def generate_qrc(directories, qrc_file):
+ with open(qrc_file, 'w') as f:
+    f.write('<RCC>\n')
+    f.write(' <qresource prefix="/">\n')
+    for directory in directories:
+        for filename in os.listdir(directory):
+            f.write(f' <file>{os.path.relpath(directory)}/{filename}</file>\n')
+    f.write(' </qresource>\n')
+    f.write('</RCC>\n')
+
+directories = [
+  r'D:\VS Code Projects (good place)\Cyber Music Streaming\ui\images', 
+  r'D:\VS Code Projects (good place)\Cyber Music Streaming\ui\album_pics',
+]
+
+generate_qrc(directories, 'resource_file.qrc')
+
 
 """
 pyuic5 -x ui\main_page\main_page.ui -o ui\main_page\main_page_ui.py
