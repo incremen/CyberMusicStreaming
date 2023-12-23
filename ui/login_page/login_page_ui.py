b21cc56de4da80e1934901afde3c292507a2d090
Itamar <s@s.com>
2023-12-23 21:46:55 +0200

    updated login page a bunch

--- ui/login_page/login_page_ui.py
+++ ui/login_page/login_page_ui.py
@@ -33,7 +33,7 @@ class Ui_MainWindow(object):
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.label = QLabel(self.groupBox)
         self.label.setObjectName(u"label")
-        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
@@ -52,12 +52,15 @@ class Ui_MainWindow(object):
         self.formLayout.setObjectName(u"formLayout")
         self.label_2 = QLabel(self.groupBox)
         self.label_2.setObjectName(u"label_2")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        sizePolicy1 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
         sizePolicy1.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
         self.label_2.setSizePolicy(sizePolicy1)
-        self.label_2.setMinimumSize(QSize(120, 100))
+        self.label_2.setMinimumSize(QSize(200, 100))
+        font1 = QFont()
+        font1.setPointSize(15)
+        self.label_2.setFont(font1)
         self.label_2.setStyleSheet(u" border: 2px solid white;\n"
 " background-color: rgba(128, 128, 128, 128);\n"
 "color: white;\n"
@@ -69,17 +72,19 @@ class Ui_MainWindow(object):
 "")
         self.label_2.setAlignment(Qt.AlignCenter)
 
-        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label_2)
+        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label_2)
 
         self.lineEdit = QLineEdit(self.groupBox)
         self.lineEdit.setObjectName(u"lineEdit")
+        sizePolicy1.setHeightForWidth(self.lineEdit.sizePolicy().hasHeightForWidth())
+        self.lineEdit.setSizePolicy(sizePolicy1)
         self.lineEdit.setMinimumSize(QSize(0, 100))
         self.lineEdit.setBaseSize(QSize(0, 100))
-        font1 = QFont()
-        font1.setPointSize(80)
-        self.lineEdit.setFont(font1)
+        font2 = QFont()
+        font2.setPointSize(80)
+        self.lineEdit.setFont(font2)
         self.lineEdit.setStyleSheet(u" border: 2px solid white;\n"
-" background-color: rgba(128, 128, 128, 128);\n"
+" background-color: transparent;\n"
 "color: white;\n"
 "border-top-right-radius: 20px;\n"
 "border-bottom-right-radius: 20px;\n"
@@ -87,13 +92,14 @@ class Ui_MainWindow(object):
 "\n"
 "")
 
-        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.lineEdit)
+        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.lineEdit)
 
         self.label_6 = QLabel(self.groupBox)
         self.label_6.setObjectName(u"label_6")
         sizePolicy1.setHeightForWidth(self.label_6.sizePolicy().hasHeightForWidth())
         self.label_6.setSizePolicy(sizePolicy1)
-        self.label_6.setMinimumSize(QSize(120, 100))
+        self.label_6.setMinimumSize(QSize(200, 100))
+        self.label_6.setFont(font1)
         self.label_6.setStyleSheet(u" border: 2px solid white;\n"
 " background-color: rgba(128, 128, 128, 128);\n"
 "color: white;\n"
@@ -104,13 +110,32 @@ class Ui_MainWindow(object):
 "")
         self.label_6.setAlignment(Qt.AlignCenter)
 
-        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.label_6)
+        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label_6)
+
+        self.lineEdit_2 = QLineEdit(self.groupBox)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        sizePolicy1.setHeightForWidth(self.lineEdit_2.sizePolicy().hasHeightForWidth())
+        self.lineEdit_2.setSizePolicy(sizePolicy1)
+        self.lineEdit_2.setMinimumSize(QSize(0, 100))
+        self.lineEdit_2.setBaseSize(QSize(0, 100))
+        self.lineEdit_2.setFont(font2)
+        self.lineEdit_2.setStyleSheet(u" border: 2px solid white;\n"
+" background-color: transparent;\n"
+"color: white;\n"
+"border-top-right-radius: 20px;\n"
+"border-bottom-right-radius: 20px;\n"
+"\n"
+"\n"
+"")
+
+        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.lineEdit_2)
 
         self.label_5 = QLabel(self.groupBox)
         self.label_5.setObjectName(u"label_5")
         sizePolicy1.setHeightForWidth(self.label_5.sizePolicy().hasHeightForWidth())
         self.label_5.setSizePolicy(sizePolicy1)
-        self.label_5.setMinimumSize(QSize(120, 100))
+        self.label_5.setMinimumSize(QSize(200, 100))
+        self.label_5.setFont(font1)
         self.label_5.setStyleSheet(u" border: 2px solid white;\n"
 " background-color: rgba(128, 128, 128, 128);\n"
 "color: white;\n"
@@ -121,44 +146,70 @@ class Ui_MainWindow(object):
 "")
         self.label_5.setAlignment(Qt.AlignCenter)
 
-        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.label_5)
-
-        self.lineEdit_2 = QLineEdit(self.groupBox)
-        self.lineEdit_2.setObjectName(u"lineEdit_2")
-        self.lineEdit_2.setMinimumSize(QSize(0, 100))
-        self.lineEdit_2.setBaseSize(QSize(0, 100))
-        self.lineEdit_2.setFont(font1)
-        self.lineEdit_2.setStyleSheet(u" border: 2px solid white;\n"
-" background-color: rgba(128, 128, 128, 128);\n"
-"color: white;\n"
-"border-top-right-radius: 20px;\n"
-"border-bottom-right-radius: 20px;\n"
-"\n"
-"")
-
-        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.lineEdit_2)
+        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.label_5)
 
         self.lineEdit_3 = QLineEdit(self.groupBox)
         self.lineEdit_3.setObjectName(u"lineEdit_3")
+        sizePolicy1.setHeightForWidth(self.lineEdit_3.sizePolicy().hasHeightForWidth())
+        self.lineEdit_3.setSizePolicy(sizePolicy1)
         self.lineEdit_3.setMinimumSize(QSize(0, 100))
         self.lineEdit_3.setBaseSize(QSize(0, 100))
-        self.lineEdit_3.setFont(font1)
+        self.lineEdit_3.setFont(font2)
         self.lineEdit_3.setStyleSheet(u" border: 2px solid white;\n"
-" background-color: rgba(128, 128, 128, 128);\n"
+" background-color: transparent;\n"
 "color: white;\n"
 "border-top-right-radius: 20px;\n"
 "border-bottom-right-radius: 20px;\n"
 "\n"
+"\n"
 "")
 
-        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.lineEdit_3)
-
-        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
-
-        self.formLayout.setItem(0, QFormLayout.LabelRole, self.horizontalSpacer)
+        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.lineEdit_3)
 
 
         self.verticalLayout.addLayout(self.formLayout)
+
+        self.groupBox_2 = QGroupBox(self.groupBox)
+        self.groupBox_2.setObjectName(u"groupBox_2")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Minimum)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.groupBox_2.sizePolicy().hasHeightForWidth())
+        self.groupBox_2.setSizePolicy(sizePolicy2)
+        self.groupBox_2.setMinimumSize(QSize(0, 0))
+        self.horizontalLayout_2 = QHBoxLayout(self.groupBox_2)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.pushButton = QPushButton(self.groupBox_2)
+        self.pushButton.setObjectName(u"pushButton")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
+        self.pushButton.setSizePolicy(sizePolicy3)
+        self.pushButton.setMinimumSize(QSize(0, 0))
+        font3 = QFont()
+        font3.setPointSize(90)
+        self.pushButton.setFont(font3)
+        self.pushButton.setStyleSheet(u" border: 2px solid pink;\n"
+" background-color: rgba(128, 128, 128, 60);\n"
+"color: white;\n"
+"border-top-left-radius: 80px;\n"
+"border-bottom-left-radius: 40px;\n"
+"border-bottom-right-radius: 80px;\n"
+"border-top-right-radius: 40px;\n"
+"\n"
+"\n"
+"")
+
+        self.horizontalLayout_2.addWidget(self.pushButton)
+
+
+        self.verticalLayout.addWidget(self.groupBox_2)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
 
 
         self.verticalLayout_2.addWidget(self.groupBox)
@@ -184,8 +235,10 @@ class Ui_MainWindow(object):
         self.label_2.setText(QCoreApplication.translate("MainWindow", u"Enter username:", None))
         self.lineEdit.setText(QCoreApplication.translate("MainWindow", u"asdf", None))
         self.label_6.setText(QCoreApplication.translate("MainWindow", u"Enter password: ", None))
-        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Insert profile picture", None))
         self.lineEdit_2.setText("")
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Insert profile picture", None))
         self.lineEdit_3.setText("")
+        self.groupBox_2.setTitle(QCoreApplication.translate("MainWindow", u"GroupBox", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"I'm ready!", None))
     # retranslateUi
 
