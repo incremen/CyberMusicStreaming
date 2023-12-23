b5cab9469c24dea9a0eca61e463e02fe61621618
Itamar <s@s.com>
2023-12-23 16:35:05 +0200

    albums pae more or less ready

--- ui/albums_page/albums_page_ui.py
+++ ui/albums_page/albums_page_ui.py
@@ -18,7 +18,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1167, 855)
+        MainWindow.resize(1086, 922)
         self.central_widget = QWidget(MainWindow)
         self.central_widget.setObjectName(u"central_widget")
         self.central_widget.setStyleSheet(u"#central_widget {\n"
@@ -36,19 +36,53 @@ class Ui_MainWindow(object):
         sizePolicy.setHeightForWidth(self.groupBox.sizePolicy().hasHeightForWidth())
         self.groupBox.setSizePolicy(sizePolicy)
         self.groupBox.setMaximumSize(QSize(16777215, 1000))
+        self.groupBox.setStyleSheet(u"#groupBox {border: none;\n"
+"}")
+        self.groupBox.setFlat(True)
         self.gridLayout_2 = QGridLayout(self.groupBox)
         self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.horizontalLayout_5 = QHBoxLayout()
+        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
+
+        self.gridLayout_2.addLayout(self.horizontalLayout_5, 0, 0, 1, 1)
+
+        self.lineEdit = QLineEdit(self.groupBox)
+        self.lineEdit.setObjectName(u"lineEdit")
+        font = QFont()
+        font.setFamily(u"Arial")
+        font.setPointSize(60)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setStyleSheet(u"QLineEdit {\n"
+" border: 2px solid gold;\n"
+" background-color: rgba(255, 255, 0, 80); \n"
+" border-radius: 30px;\n"
+" color: gold;\n"
+" font-family: Arial;\n"
+" padding: 30px;\n"
+" box-shadow: 0px 0px 10px gold;\n"
+"}\n"
+"\n"
+"QLineEdit:focus {\n"
+" outline: 2px solid gold;\n"
+"}\n"
+"")
+
+        self.gridLayout_2.addWidget(self.lineEdit, 1, 0, 1, 1)
+
         self.album_grid = QGridLayout()
         self.album_grid.setObjectName(u"album_grid")
-        self.pushButton_2 = QPushButton(self.groupBox)
-        self.pushButton_2.setObjectName(u"pushButton_2")
+        self.pushButton_7 = QPushButton(self.groupBox)
+        self.pushButton_7.setObjectName(u"pushButton_7")
         sizePolicy1 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Expanding)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.pushButton_2.sizePolicy().hasHeightForWidth())
-        self.pushButton_2.setSizePolicy(sizePolicy1)
-        self.pushButton_2.setMaximumSize(QSize(200, 16777215))
-        self.pushButton_2.setStyleSheet(u" border: 2px solid white;\n"
+        sizePolicy1.setHeightForWidth(self.pushButton_7.sizePolicy().hasHeightForWidth())
+        self.pushButton_7.setSizePolicy(sizePolicy1)
+        self.pushButton_7.setMaximumSize(QSize(200, 16777215))
+        font1 = QFont()
+        font1.setPointSize(40)
+        self.pushButton_7.setFont(font1)
+        self.pushButton_7.setStyleSheet(u" border: 2px solid white;\n"
 " background-color: rgba(128, 128, 128, 128);\n"
 " border-radius: 30px;\n"
 "color: white;\n"
@@ -56,31 +90,17 @@ class Ui_MainWindow(object):
 "\n"
 "")
 
-        self.album_grid.addWidget(self.pushButton_2, 0, 2, 1, 1)
-
-        self.pushButton_3 = QPushButton(self.groupBox)
-        self.pushButton_3.setObjectName(u"pushButton_3")
-        sizePolicy2 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
-        sizePolicy2.setHorizontalStretch(0)
-        sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(self.pushButton_3.sizePolicy().hasHeightForWidth())
-        self.pushButton_3.setSizePolicy(sizePolicy2)
-        self.pushButton_3.setMaximumSize(QSize(200, 16777215))
-        self.pushButton_3.setStyleSheet(u" border: 2px solid white;\n"
-" background-color: rgba(128, 128, 128, 128);\n"
-" border-radius: 30px;\n"
-"color: white;\n"
-"\n"
-"\n"
-"")
-
-        self.album_grid.addWidget(self.pushButton_3, 0, 1, 1, 1)
+        self.album_grid.addWidget(self.pushButton_7, 0, 3, 1, 1)
 
         self.pushButton = QPushButton(self.groupBox)
         self.pushButton.setObjectName(u"pushButton")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
         sizePolicy2.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
         self.pushButton.setSizePolicy(sizePolicy2)
         self.pushButton.setMaximumSize(QSize(200, 16777215))
+        self.pushButton.setFont(font1)
         self.pushButton.setStyleSheet(u" border: 2px solid white;\n"
 " background-color: rgba(128, 128, 128, 128);\n"
 " border-radius: 30px;\n"
@@ -91,11 +111,44 @@ class Ui_MainWindow(object):
 
         self.album_grid.addWidget(self.pushButton, 0, 0, 1, 1)
 
+        self.pushButton_6 = QPushButton(self.groupBox)
+        self.pushButton_6.setObjectName(u"pushButton_6")
+        sizePolicy1.setHeightForWidth(self.pushButton_6.sizePolicy().hasHeightForWidth())
+        self.pushButton_6.setSizePolicy(sizePolicy1)
+        self.pushButton_6.setMaximumSize(QSize(200, 16777215))
+        self.pushButton_6.setFont(font1)
+        self.pushButton_6.setStyleSheet(u" border: 2px solid white;\n"
+" background-color: rgba(128, 128, 128, 128);\n"
+" border-radius: 30px;\n"
+"color: white;\n"
+"\n"
+"\n"
+"")
+
+        self.album_grid.addWidget(self.pushButton_6, 0, 2, 1, 1)
+
+        self.pushButton_9 = QPushButton(self.groupBox)
+        self.pushButton_9.setObjectName(u"pushButton_9")
+        sizePolicy1.setHeightForWidth(self.pushButton_9.sizePolicy().hasHeightForWidth())
+        self.pushButton_9.setSizePolicy(sizePolicy1)
+        self.pushButton_9.setMaximumSize(QSize(200, 16777215))
+        self.pushButton_9.setFont(font1)
+        self.pushButton_9.setStyleSheet(u" border: 2px solid white;\n"
+" background-color: rgba(128, 128, 128, 128);\n"
+" border-radius: 30px;\n"
+"color: white;\n"
+"\n"
+"\n"
+"")
+
+        self.album_grid.addWidget(self.pushButton_9, 1, 0, 1, 1)
+
         self.pushButton_4 = QPushButton(self.groupBox)
         self.pushButton_4.setObjectName(u"pushButton_4")
         sizePolicy1.setHeightForWidth(self.pushButton_4.sizePolicy().hasHeightForWidth())
         self.pushButton_4.setSizePolicy(sizePolicy1)
         self.pushButton_4.setMaximumSize(QSize(200, 16777215))
+        self.pushButton_4.setFont(font1)
         self.pushButton_4.setStyleSheet(u" border: 2px solid white;\n"
 " background-color: rgba(128, 128, 128, 128);\n"
 " border-radius: 30px;\n"
@@ -104,29 +157,58 @@ class Ui_MainWindow(object):
 "\n"
 "")
 
-        self.album_grid.addWidget(self.pushButton_4, 0, 3, 1, 1)
+        self.album_grid.addWidget(self.pushButton_4, 0, 1, 1, 1)
+
+        self.pushButton_13 = QPushButton(self.groupBox)
+        self.pushButton_13.setObjectName(u"pushButton_13")
+        sizePolicy1.setHeightForWidth(self.pushButton_13.sizePolicy().hasHeightForWidth())
+        self.pushButton_13.setSizePolicy(sizePolicy1)
+        self.pushButton_13.setMaximumSize(QSize(200, 16777215))
+        self.pushButton_13.setFont(font1)
+        self.pushButton_13.setStyleSheet(u" border: 2px solid white;\n"
+" background-color: rgba(128, 128, 128, 128);\n"
+" border-radius: 30px;\n"
+"color: white;\n"
+"\n"
+"\n"
+"")
+
+        self.album_grid.addWidget(self.pushButton_13, 1, 1, 1, 1)
+
+        self.pushButton_14 = QPushButton(self.groupBox)
+        self.pushButton_14.setObjectName(u"pushButton_14")
+        sizePolicy1.setHeightForWidth(self.pushButton_14.sizePolicy().hasHeightForWidth())
+        self.pushButton_14.setSizePolicy(sizePolicy1)
+        self.pushButton_14.setMaximumSize(QSize(200, 16777215))
+        self.pushButton_14.setFont(font1)
+        self.pushButton_14.setStyleSheet(u" border: 2px solid white;\n"
+" background-color: rgba(128, 128, 128, 128);\n"
+" border-radius: 30px;\n"
+"color: white;\n"
+"\n"
+"\n"
+"")
+
+        self.album_grid.addWidget(self.pushButton_14, 1, 2, 1, 1)
+
+        self.pushButton_12 = QPushButton(self.groupBox)
+        self.pushButton_12.setObjectName(u"pushButton_12")
+        sizePolicy1.setHeightForWidth(self.pushButton_12.sizePolicy().hasHeightForWidth())
+        self.pushButton_12.setSizePolicy(sizePolicy1)
+        self.pushButton_12.setMaximumSize(QSize(200, 16777215))
+        self.pushButton_12.setFont(font1)
+        self.pushButton_12.setStyleSheet(u" border: 2px solid white;\n"
+" background-color: rgba(128, 128, 128, 128);\n"
+" border-radius: 30px;\n"
+"color: white;\n"
+"\n"
+"\n"
+"")
+
+        self.album_grid.addWidget(self.pushButton_12, 1, 3, 1, 1)
 
 
-        self.gridLayout_2.addLayout(self.album_grid, 1, 0, 1, 1)
-
-        self.horizontalLayout_5 = QHBoxLayout()
-        self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
-        self.textEdit = QTextEdit(self.groupBox)
-        self.textEdit.setObjectName(u"textEdit")
-        sizePolicy3 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Minimum)
-        sizePolicy3.setHorizontalStretch(0)
-        sizePolicy3.setVerticalStretch(0)
-        sizePolicy3.setHeightForWidth(self.textEdit.sizePolicy().hasHeightForWidth())
-        self.textEdit.setSizePolicy(sizePolicy3)
-        self.textEdit.setMinimumSize(QSize(0, 200))
-        font = QFont()
-        font.setPointSize(100)
-        self.textEdit.setFont(font)
-
-        self.horizontalLayout_5.addWidget(self.textEdit)
-
-
-        self.gridLayout_2.addLayout(self.horizontalLayout_5, 0, 0, 1, 1)
+        self.gridLayout_2.addLayout(self.album_grid, 2, 0, 1, 1)
 
 
         self.horizontalLayout_6.addWidget(self.groupBox)
@@ -134,7 +216,7 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.central_widget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 1167, 21))
+        self.menubar.setGeometry(QRect(0, 0, 1086, 21))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -148,14 +230,38 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
         self.groupBox.setTitle("")
-        self.pushButton_2.setText(QCoreApplication.translate("MainWindow", u"Album c", None))
-        self.pushButton_3.setText(QCoreApplication.translate("MainWindow", u"Album b", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Album a", None))
-        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"Album c", None))
-        self.textEdit.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
-"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
-"p, li { white-space: pre-wrap; }\n"
-"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:100pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:72pt;\">Find a song...</span></p></body></html>", None))
+        self.lineEdit.setText(QCoreApplication.translate("MainWindow", u"Find a song...", None))
+        self.pushButton_7.setText(QCoreApplication.translate("MainWindow", u"Summer \n"
+"\n"
+"\n"
+"hits", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Popular \n"
+"\n"
+"\n"
+"albums", None))
+        self.pushButton_6.setText(QCoreApplication.translate("MainWindow", u"Summer \n"
+"\n"
+"\n"
+"albums", None))
+        self.pushButton_9.setText(QCoreApplication.translate("MainWindow", u"asdf \n"
+"\n"
+"\n"
+"asdf", None))
+        self.pushButton_4.setText(QCoreApplication.translate("MainWindow", u"Top \n"
+"\n"
+"\n"
+"hits", None))
+        self.pushButton_13.setText(QCoreApplication.translate("MainWindow", u"asdf \n"
+"\n"
+"\n"
+"asdf", None))
+        self.pushButton_14.setText(QCoreApplication.translate("MainWindow", u"asdf \n"
+"\n"
+"\n"
+"asdf", None))
+        self.pushButton_12.setText(QCoreApplication.translate("MainWindow", u"asdf \n"
+"\n"
+"\n"
+"asdf", None))
     # retranslateUi
 
