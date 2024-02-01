4e6575049afdcfcc0a865c0b993da73e2fb358ae
Itamar <s@s.com>
2024-02-01 20:03:43 +0200

    reset run_server apparently i deleted some stuff at some point

--- run_server.py
+++ run_server.py
@@ -5,8 +5,14 @@ import custom_logging
 from music_playing.convert_to_wav import convert_mp3s_in_directory_to_wav
 from server.hls_server import start_hls_server
 def main():
-    numbers = [1,1,2,2,3,3]
-    unique_nums = [number for number in numbers if number not in unique_nums]
+    custom_logger = custom_logging.CustomLogger(log_files=["server.log"])
+    custom_logger.clear_logs()
+    songs_dir = os.path.abspath(r"songs")
+    server_thread = threading.Thread(target=start_hls_server)
+    server_thread.start()
+    
+    server_socket_handler = ServerSocketHandler(songs_dir)
+    server_socket_handler.start()
     
     
 
