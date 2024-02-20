32f164c15927f52c9639db7953e6c221a8c10c31
Itamar <s@s.com>
2024-02-20 20:34:01 +0200

    added Song model

--- database/models.py
+++ database/models.py
@@ -1,5 +1,5 @@
 # models.py
-from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey, Float
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -24,9 +24,24 @@ class Playlist(Base):
     user_id = Column(Integer, ForeignKey('users.id'))
 
     user = relationship('User', back_populates='playlists')
+    songs = relationship('Song', back_populates='playlist')
 
     def __repr__(self):
         return f"<Playlist(id={self.id}, items={self.items}, user_id={self.user_id})>"
-    
-    def get_as_list(self):
-        return eval(self.items)
+
+
+class Song(Base):
+    __tablename__ = 'songs'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    length = Column(Float)
+    nframes = Column(Integer)
+    framerate = Column(Integer)
+    nchannels = Column(Integer)
+    playlist_id = Column(Integer, ForeignKey('playlists.id'))
+
+    playlist = relationship('Playlist', back_populates='songs')
+
+    def __repr__(self):
+        return f"<Song(id={self.id}, name={self.name}, length={self.length}, nframes={self.nframes}, 
+    framerate={self.framerate}, nchannels={self.nchannels}, playlist_id={self.playlist_id})>"
