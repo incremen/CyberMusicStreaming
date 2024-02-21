bfa00a1353736f599bbdf83beaae5125f0aa4742
Itamar <s@s.com>
2024-02-21 13:20:46 +0200

    deleted unecessary

--- database/client_db_funcs.py
+++ database/client_db_funcs.py
@@ -1,11 +1,11 @@
 from database.models import User, Playlist, Base
-from database.operations import add_song_to_playlist, remove_song_from_playlist
 import database.utils as utils
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from custom_logging import CustomLogger
 
 DATABASE_URL = "sqlite:///database/test_db.db"
+
 
 
 def create_playlist_for_user(session, playlist_items : list):
@@ -38,6 +38,10 @@ def create_session():
     engine = create_engine(DATABASE_URL)
     session = Session(bind=engine)
     return session
+
+
+def get_first_user(session):
+    return session.query(User).first()
 
 
 def get_engine():
--- testing.py
+++ testing.py
@@ -1,11 +1,12 @@
 from database.models import User, Playlist, Base, Song
-from database.operations import add_song_to_playlist, remove_song_from_playlist
 import database.utils as utils
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from custom_logging import CustomLogger
 from database import client_db_funcs
 import logging
+
+
 def main():
     custom_logger = CustomLogger(log_files=["testing.log"]) 
     custom_logger.clear_logs()
@@ -14,6 +15,7 @@ def main():
     create_dummy_data(session)
     
     utils.log_all_users_playlists(session)
+
 
 def create_dummy_data(session):
     user1 = User(username="user1")
