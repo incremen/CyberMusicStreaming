a4b4e4e0d81a3da096f7ba64b194d88d5b7bdf79
Itamar <s@s.com>
2024-04-03 15:58:45 +0300

    reset_db now adds all the basic songs

--- database/__init__.py
+++ database/__init__.py
@@ -1,2 +1,5 @@
+import os
+
 DB_PATH = r"D:\vs_code_projects_good_place\cyber_music_streaming\database\test_db.db"
 SQLITE_PATH = fr"sqlite:///{DB_PATH}?check_same_thread=False"
+SONG_DIR = os.path.abspath(r"songs")
--- log_db.py
+++ log_db.py
@@ -1,10 +1,11 @@
 from database.models import Base
 from custom_logging import CustomLogger
-from database.utils import log_all_users_playlists, create_session
+from database.utils import log_all_users_playlists, create_session, log_all_songs
 
 def log_db():
     CustomLogger()
     log_all_users_playlists(create_session())
+    log_all_songs(create_session())
 
 
 if __name__ == "__main__":
