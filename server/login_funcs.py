e52a458ed32ad6ef8022cc167ad82b2af9244b52
Itamar <s@s.com>
2024-04-06 21:15:08 +0300

    add create new account and login handlers for server

---
+++ server/login_funcs.py
@@ -0,0 +1,57 @@
+import logging
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database.models import User  # Assuming you have a User model defined elsewhere
+from log_db import log_db
+from result import Ok, Err, Result, is_ok, is_err
+from database.utils import create_session
+
+def create_new_account(username, password) -> Result:
+    logging.info(f"Attempting to create a new account for user: {username}")
+    session = create_session()
+    existing_user = session.query(User).filter_by(username=username).first()
+
+    if existing_user:
+        error_message = f"User {username} already exists"
+        logging.error(error_message)
+        session.close()
+        return Err(error_message)
+
+    new_user = User(username=username, password=password)
+    session.add(new_user)
+    session.commit()
+    ok_message = f"Account for user {username} created successfully"
+    logging.info(ok_message)
+    log_db()
+    session.close()
+    return Ok(ok_message)
+
+def login(username, password) -> Result:
+    logging.info(f"Attempting to log in user: {username}")
+    session = create_session()
+    user: User = session.query(User).filter_by(username=username).first()
+
+    if not user:
+        error_message = f"User {username} not found"
+        logging.error(error_message)
+        session.close()
+        return Err(error_message)
+
+    if user.password != password:
+        error_message = f"Wrong password"
+        logging.error(error_message)
+        session.close()
+        return Err(error_message)
+
+    ok_msg = f"User {username} logged in successfully"
+    logging.info(ok_msg)
+    log_db()
+    session.close()
+    return Ok(ok_msg)
+
+def logout(self) -> Result:
+    current_user = self.get_current_user()
+    logging.info(f"Logging out user: {current_user.username}")
+    self.current_user_id = None
+    logging.info("User logged out successfully")
+    return Ok(True)
--- server/server_socket.py
+++ server/server_socket.py
@@ -12,6 +12,7 @@ import pprint
 from dataclasses import asdict
 from backend import server_addr_tuple
 from database import utils
+from server import login_funcs
 #sid - socket id
 # environ is a dictionary that contains environmental information related to the incoming connection
 
@@ -33,6 +34,41 @@ class ServerSocketHandler:
         def send_song_list(sid):
             song_dict_list = [asdict(song) for song in self.song_list]
             self.sio.emit("song_list", song_dict_list, room=sid)
+
+        @self.sio.on("create_new_account")
+        def create_new_account_handler(sid, data):
+            username = data.get('username')
+            password = data.get('password')
+            
+            result = login_funcs.create_new_account(username, password)
+            
+            if result.is_ok():
+                with self.sio.session(sid) as session_data:
+                    session_data['username'] = username
+                    
+            result_msg = result.unwrap()
+                                
+            self.sio.emit("account_create_result", {"result": result.is_ok(), "message" : result_msg},  room=sid)
+
+        @self.sio.on("login")
+        def login_handler(sid, data):
+            username = data.get('username')
+            password = data.get('password')
+            
+            result = login_funcs.login(username, password)
+            
+            if result.is_ok():
+                with self.sio.session(sid) as session_data:
+                    session_data['username'] = username
+                    
+            self.sio.emit("login_result", {"result": result.is_ok(), "message": result.unwrap()}, room=sid)
+
+        @self.sio.on("logout")
+        def logout_handler(sid):
+            with self.sio.session(sid) as session_data:
+                session_data.clear()
+            # self.sio.emit("logout_success", {"message": "Logged out successfully"}, room=sid)
+            
             
         app = socketio.WSGIApp(self.sio)
         wsgi.server(eventlet.listen(server_addr_tuple), app)
