681b8354b3908b044324d19ea282b3f0ae2d3ef4
Itamar <s@s.com>
2024-06-15 18:07:33 +0300

    verified hls server works with ngrok

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,3 @@
-# server_addr_tuple = ('localhost', 12345)
-
-# CLIENT_CONNECTS_TO_STR = "tcp://2.tcp.eu.ngrok.io:12828"
-
 CLIENT_CONNECTS_TO_STR = "wss://localhost:12345"
 # CLIENT_CONNECTS_TO_STR = " tcp://6.tcp.eu.ngrok.io:16758"
 
@@ -10,6 +6,8 @@ SERVER_ADDR_TUPLE = ('localhost', 12345)
 
 SERVER_HLS_HOST, SERVER_HLS_PORT = 'localhost', 8080
 CLIENT_HLS_HOST, CLIENT_HLS_PORT = 'localhost', 8080
+# CLIENT_HLS_HOST, CLIENT_HLS_PORT = '0.tcp.eu.ngrok.io', 14483
+
 
 SERVER_CRT_PATH = r"D:\vs_code_projects_good_place\cyber_music_streaming\certificate\server.crt"
 SERVER_KEY_PATH = r"D:\vs_code_projects_good_place\cyber_music_streaming\certificate\server.key"
