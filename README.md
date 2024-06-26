6e2f6b0e263537eb46887529605cb0f10e794c22
royo <roy@osherove.com>
2024-06-26 18:23:42 +0300

    change ngrok

---
+++ README.md
@@ -0,0 +1,5 @@
+#starting a server
+To get ngrok to do the forwarding you want, do this:
+
+ngrok start --config=./backend/ngrok_config.yaml --all
+You can do the absolute path, file is in the same folder as this file
--- backend/__init__.py
+++ backend/__init__.py
@@ -11,8 +11,3 @@ CLIENT_HLS_HOST, CLIENT_HLS_PORT = 'localhost', 8000
 
 SERVER_CRT_PATH = r"../certificate/server.crt"
 SERVER_KEY_PATH = r"../certificate/server.key"
-"""
-To get ngrok to do the forwarding you want, do this:
-ngrok start --config=D:\vs_code_projects_good_place\cyber_music_streaming\backend\ngrok_config.yaml --all
-You can do the absolute path, file is in the same folder as this file
-"""
