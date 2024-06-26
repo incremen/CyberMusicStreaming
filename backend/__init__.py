60e77b12e96b683699004b96483e8131b7d74c25
Itamar <s@s.com>
2024-06-26 12:22:36 +0300

    added explanation on how to use and moved ngrok_config

--- backend/__init__.py
+++ backend/__init__.py
@@ -11,3 +11,9 @@ CLIENT_HLS_HOST, CLIENT_HLS_PORT = '0.tcp.eu.ngrok.io', 14347
 
 SERVER_CRT_PATH = r"D:\vs_code_projects_good_place\cyber_music_streaming\certificate\server.crt"
 SERVER_KEY_PATH = r"D:\vs_code_projects_good_place\cyber_music_streaming\certificate\server.key"
+
+"""
+To get ngrok to do the forwarding you want, do this:
+ngrok start --config=D:\vs_code_projects_good_place\cyber_music_streaming\backend\ngrok_config.yaml --all
+You can do the absolute path, file is in the same folder as this file
+"""
