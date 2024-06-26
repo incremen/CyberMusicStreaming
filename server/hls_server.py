d4e399d9c2d488792a86c500b65fd95bae5501ec
royo <roy@osherove.com>
2024-06-26 18:54:20 +0300

    bug fix on server streaming

--- server/hls_server.py
+++ server/hls_server.py
@@ -8,28 +8,35 @@ import ssl
 
 class HLSHandler(SimpleHTTPRequestHandler):
     def translate_path(self, requested_path):
-        path_with_spaces = requested_path.replace('%20', ' ')
-        segments_dir = Path(r'D:\vs_code_projects_good_place\cyber_music_streaming\song_segments')
-        return_path = segments_dir / Path(path_with_spaces).relative_to('/')
-        abs_return_path = return_path.absolute()
-        logging.info(f"Requested path: {path_with_spaces}")
-        logging.debug(f"Return path: {abs_return_path}")
-        logging.debug(f"Return path exists: {abs_return_path.exists()}")
-        return str(abs_return_path)
-
+        try:
+            project_root = Path(__file__).resolve().parent.parent
+            path_with_spaces = requested_path.replace('%20', ' ')
+            return_path = str(project_root) + '/song_segments/' + path_with_spaces
+            logging.info(f"Requested path: {path_with_spaces}")
+            logging.info(f"project_root: {project_root}")
+            logging.info(f"Return path: {return_path}")
+            return str(return_path)
+        except Exception as e:
+            logging.error(f"Error in translate_path: {str(e)}")
+            return None
 
 def start_hls_server():
-    logging.info("Starting HLS server..")
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    
-    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-    
-    logging.info(f"Loading certificate from: {os.path.abspath(SERVER_CRT_PATH)}")
-    logging.info(f"Loading key from: {os.path.abspath(SERVER_KEY_PATH)}")
-    context.load_cert_chain(SERVER_CRT_PATH, SERVER_KEY_PATH)
-    
-    with HTTPServer((SERVER_HLS_HOST, SERVER_HLS_PORT), HLSHandler) as httpd:
-        httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+    try:
+        logging.info("Starting HLS server..")
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
         
-        print(f'Serving at https://{SERVER_HLS_HOST}:{SERVER_HLS_PORT}')
-        httpd.serve_forever()
+        context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        
+        logging.info(f"Loading certificate from: {os.path.abspath(SERVER_CRT_PATH)}")
+        logging.info(f"Loading key from: {os.path.abspath(SERVER_KEY_PATH)}")
+        context.load_cert_chain(SERVER_CRT_PATH, SERVER_KEY_PATH)
+        
+        with HTTPServer((SERVER_HLS_HOST, SERVER_HLS_PORT), HLSHandler) as httpd:
+            httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+            
+            print(f'Serving at https://{SERVER_HLS_HOST}:{SERVER_HLS_PORT}')
+            httpd.serve_forever()
+    except Exception as e:
+        logging.error(f"Error starting HLS server: {str(e)}")
+        logging.info("Clearing logs due to error...")
+        CustomLogger.clear_logs()
